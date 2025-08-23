2fa12e7453dc5e76ca8e7f2604c548e715e0c0cf
Douglas Menezes <goulartdouglas4@gmail.com>
2025-08-23 16:57:53 -0300

    homolog ok..

--- src/main/java/com/douglasgm/gameapi/repository/GameListRepository.java
+++ src/main/java/com/douglasgm/gameapi/repository/GameListRepository.java
@@ -1,11 +1,7 @@
 package com.douglasgm.gameapi.repository;
 
-import com.douglasgm.gameapi.dslist.GameMinProjection;
 import com.douglasgm.gameapi.entity.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 
