3223d97e0d419f87b09743a4cfe52dcfadf5ae16
Douglas Menezes <goulartdouglas4@gmail.com>
2025-08-23 17:44:51 -0300

    add new class to web config..

--- src/main/java/com/douglasgm/gameapi/repository/GameListRepository.java
+++ src/main/java/com/douglasgm/gameapi/repository/GameListRepository.java
@@ -2,7 +2,13 @@ package com.douglasgm.gameapi.repository;
 
 import com.douglasgm.gameapi.entity.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE game_list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long gameListId, Long gameId, Integer newPosition);
 
 }
